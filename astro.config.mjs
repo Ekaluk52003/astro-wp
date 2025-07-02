948ad6043e31735679659af49414c9c0bfad8d6b
Ekaluk52003 <ekaluk.p@gmail.com>
2025-07-02 21:49:29 +0700

    update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
